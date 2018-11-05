9e4c44a4c5a8db87a64e7342a86fe5bb46e2b375
daniellatali <43752104+daniellatali@users.noreply.github.com>
2018-11-05 00:35:26 -0500

    Add files via upload

---
+++ ex22.py
@@ -0,0 +1,30 @@
+# <-- Symbol used for making comments and doesn't show in terminal
+# print() <-- print means to display what text or values in the terminal
+# + add
+# - subtract
+# / divide
+# * multiply
+# % modulus (rounds to nearest integer w/o remainder)
+# < less than
+# > greater than
+# = equal to (also can assign variables)
+# <= less than or equal to
+# >= greater than or equal to
+
+# f before string "" <-- Tells python that string needs to be formatted and to put the variables in there
+# print(variable.format(1, 2, 3, 4)) <-- the format command
+# variable = input() <-- prompts user in terminal to input a specific variable.
+#  \t <-- makes indentation on text while displaying in the terminal
+# \n <-- makes new line for text while displaying in the terminal
+# from ... import... <-- uses or applies features in the command
+# argv <-- argument variable: holds arguments to pass through your python script
+# print(txt.read()) <-- Displays a text file when inputted
+# open() <-- opens any file or document you want in the terminal
+# open(  ,'w') opens file in write mode
+# open(  ,'r') opens file in read mode
+# open(  , 'a') opens file in append mode
+# variable.truncate <-- truncating the file means to shorten it
+# variable.close <-- closes the file
+# len() <-- gets length of string that you pass to it and returns that as a number
+# def variable: <-- a way to define functions
+# f.seek(any number) <-- seek means it opens a file to whatever point in bytes to read or write at that pointself.
